c5d01bd47aa507a2f3f72bb42f84f528f8ea6d21
jdaguilar <jorgeaguilar2761@gmail.com>
2025-01-07 22:48:02 -0500

    edit queries homework 2

--- bootcamp/materials/2-fact-data-modeling/homework/sql/1_deduplicate_game_details.sql
+++ bootcamp/materials/2-fact-data-modeling/homework/sql/1_deduplicate_game_details.sql
@@ -1,7 +1,7 @@
 -- deduplicate_game_details.sql
 
 WITH game_details_deduped AS (
-    SELECT *, ROW_NUMBER() OVER(PARTITION BY game_id, team_id, player_id) as row_num
-	FROM game_details
+    SELECT *, ROW_NUMBER() OVER(PARTITION BY game_id, team_id, player_id ORDER BY some_column) as row_num
+    FROM game_details
 )
-select * FROM game_details_deduped WHERE row_num > 1
+SELECT * FROM game_details_deduped WHERE row_num = 1;
